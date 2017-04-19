0dcabc4c34ca54f79e33d14a7201a78c015fd14a
wasilevskiy <vasilevskii_ilya@mail.ru>
2017-04-19 12:45:07 +0300

    Доделал

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -39,5 +39,49 @@ namespace LanguageFeatures.Controllers
             decimal cartTotal = cart.TotalPrices();
             return View("Result", (object)String.Format("Total: {0:c}", cartTotal));
         }
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product> {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+            // создание и заполнение массива объектов Product
+            Product[] productArray = {
+            new Product {Name = "Kayak", Price = 275M},
+            new Product {Name = "Lifejacket", Price = 48.95M},
+            new Product {Name = "Soccer ball", Price = 19.50M},
+            new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            // получение общей стоимости продуктов в корзине
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
+            return View("Result",
+            (object)String.Format("Cart Total: {0}, Array Total: {1}",
+            cartTotal, arrayTotal));
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            // создаем и заполняем ShoppingCart
+            IEnumerable<Product> products = new ShoppingCart {
+            Products = new List<Product> {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            }
+            };
+            Func<Product, bool> categoryFilter = prod => prod.Category == "Soccer";
+            decimal total = 0;
+            foreach (Product prod in products.Filter(prod=>prod.Category=="Soccer" || prod.Price >20)) {
+            total += prod.Price;
+            }
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
     }
 }
